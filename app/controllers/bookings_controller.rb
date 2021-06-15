656c0f6e2019f487fe3db5c8718edf7a48d1669a
Audrey Gonera <audrey.gonera@gmail.com>
2021-06-15 12:44:31 +0200

    navbar done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,10 @@
 class BookingsController < ApplicationController
 
   def new
-    @animal = Animal.find([:animal_id])
+    @animal = Animal.find(params[:animal_id])
     @booking = Booking.new
   end
 
 end
-
 
 #créer les méthodes manquantes
